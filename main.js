7cc2f73ffd8e39fa307fba6e704d931242463db1
Maxime <maximebarber@gmail.com>
2023-04-29 11:16:16 +0200

    Finished!

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function talkieWalkie(content) {
 function getLatestPost(posts) {
   console.log(posts.slice(-1).pop())
 }
+
+document.addEventListener("alpine:init", () => {
+  Alpine.store("darkMode", {
+    on: false,
+
+    toggle() {
+      this.on = !this.on
+    }
+  })
+})
